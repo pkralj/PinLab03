89f3430b280162922f87248c3b478ca574513991
Patrik Kralj <patrikkralj97@gmail.com>
2023-11-14 16:11:52 +0100

    initial commit

---
+++ MVCCar/Data/MVCCarContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class MVCCarContext : DbContext
+{
+    public MVCCarContext(DbContextOptions<MVCCarContext> options)
+        : base(options)
+    {
+    }
+
+
+
+    public DbSet<MVCCar.Models.Car> Car { get; set; } = default!;
+}
---
+++ MVCCar/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace MVCCar.Models
+{
+	public class Car
+	{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public string Color { get; set; }
+        public int ProductionYear { get; set; }
+        public decimal Price { get; set; }
+        
+    }
+}
+
